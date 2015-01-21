f83abdecea5c159541814ba7f3aab062060e16bb
Michaël Fortin <fortinmike@irradiated.net>
2015-01-21 16:54:15 -0500

    Added MultiRepo::Git to simplify running Git commands in either the current directory or some other working copy.

---
+++ lib/multirepo/git.rb
@@ -0,0 +1,26 @@
+require_relative "runner"
+
+module MultiRepo
+  class Git
+    def self.run(*args)
+      # No method overloading makes me sad
+      if args.length == 2
+        self.run_in_current_dir(*args)
+      elsif args.length == 3
+        self.run_in_working_dir(*args)
+      else
+        raise
+      end
+    end
+    
+    def self.run_in_current_dir(command, show_output = true)
+      full_command = "git #{command}"
+      MultiRepo::Runner.run(full_command, show_output)
+    end
+    
+    def self.run_in_working_dir(working_dir, command, show_output = true)
+      full_command = "git --git-dir=\"#{working_dir}/.git\" --work-tree=\"#{working_dir}\" #{command}";
+      MultiRepo::Runner.run(full_command, show_output)
+    end
+  end
+end
