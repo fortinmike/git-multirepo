b98a90792ba36da09496f2576ff4c079029876f8
Michaël Fortin <fortinmike@irradiated.net>
2015-01-21 17:29:49 -0500

    Implemented multi fetch.

--- lib/multirepo.rb
+++ lib/multirepo.rb
@@ -1,2 +1,3 @@
 require_relative "multirepo/command"
 require_relative "multirepo/commands/setup"
+require_relative "multirepo/commands/fetch"
---
+++ lib/multirepo/commands/fetch.rb
@@ -0,0 +1,23 @@
+require "claide"
+
+require_relative "../loader"
+require_relative "../console"
+
+module MultiRepo
+  class Fetch < MultiRepo::Command
+    self.command = "fetch"
+    self.summary = "Performs a git fetch on all repositories."
+    
+    def initialize(argv)
+      super
+    end
+    
+    def run
+      MultiRepo::Console.log_step("Fetching repositories...")
+      
+      @repos.each(&:fetch)
+      
+      MultiRepo::Console.log_step("Done!")
+    end
+  end
+end
