80fcb2deac102ab351034fd581db74dc9bcea3a8
Michaël Fortin <fortinmike@irradiated.net>
2015-01-23 16:39:42 -0500

    The install command now installs a bundled pre-commit hook in the main repo. The hook itself is not implemented. It will make sure we don't commit the main repo before committing dependencies.

--- lib/multirepo.rb
+++ lib/multirepo.rb
@@ -1,3 +1,11 @@
 require_relative "multirepo/command"
 require_relative "multirepo/commands/install"
 require_relative "multirepo/commands/fetch"
+
+module MultiRepo
+  class MultiRepo
+    def self.path
+      Gem::Specification.find_by_name("git-multirepo").gem_dir
+    end
+  end
+end
--- lib/multirepo/commands/install.rb
+++ lib/multirepo/commands/install.rb
@@ -1,6 +1,8 @@
 require "claide"
 
+require "multirepo"
 require "multirepo/utility/console"
+require "multirepo/git/repo"
 require "multirepo/loader"
 
 module MultiRepo
@@ -18,10 +20,16 @@ module MultiRepo
       Console.log_step("Setupping multiple repositories...")
       
       @entries.each(&:install)
+      install_pre_commit_hook
       
       Console.log_step("Done!")
     rescue Exception => e
       Console.log_error(e.message)
+    end
+    
+    def install_pre_commit_hook
+      hook_path = File.join(MultiRepo.path, "hooks/pre-commit")
+      FileUtils.cp(hook_path, ".git/hooks")
     end
   end
 end
