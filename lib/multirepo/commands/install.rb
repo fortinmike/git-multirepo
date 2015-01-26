f845707065b9b97c3df92004c0caf443af23482b
Michaël Fortin <fortinmike@irradiated.net>
2015-01-26 00:10:52 -0500

    Moved installation logic out of ConfigEntry and into Install (command).

--- lib/multirepo/commands/install.rb
+++ lib/multirepo/commands/install.rb
@@ -13,13 +13,59 @@ module MultiRepo
       Console.log_step("Cloning dependencies and installing hooks...")
       
       self.load_entries  
-      @entries.each(&:install)
+      @entries.each { |e| install(e) }
       
       self.install_pre_commit_hook
       
       Console.log_step("Done!")
     rescue Exception => e
       Console.log_error(e.message)
+    end
+    
+    def install(entry)
+      if entry.repo.exists?
+        check_repo_validity(entry)
+        fetch_repo(entry)
+      else
+        clone_repo(entry)
+      end
+      checkout_branch(entry)
+    end
+    
+    # Repo operations
+    
+    def fetch_repo(entry)
+      Console.log_substep("Working copy #{entry.repo.working_copy} already exists, fetching instead...")
+      if !entry.repo.fetch then raise "Could not fetch from remote #{entry.repo.remote('origin').url}"
+      end
+    end
+    
+    def clone_repo(entry)
+      Console.log_substep("Cloning #{entry.remote_url} to #{entry.repo.working_copy}")
+      if !entry.repo.clone(entry.remote_url) then raise "Could not clone remote #{entry.remote_url}" end
+    end
+    
+    def checkout_branch(entry)
+      branch = entry.repo.branch(entry.branch_name);
+      
+      unless branch.exists?
+        Console.log_warning("Branch #{entry.branch_name} doesn't exist in working copy #{entry.repo.working_copy}")
+        return
+      end
+      
+      if branch.checkout
+        Console.log_info("Checked out branch #{branch.name} -> origin/#{branch.name}")
+      else
+        raise "Could not setup branch #{branch.name}"
+      end
+    end
+    
+    # Validation
+    
+    def check_repo_validity(entry)
+      unless entry.repo.remote("origin").url == entry.remote_url
+        raise "#{entry.folder_name} origin URL (#{entry.repo.remote('origin').url}) does not match entry (#{entry.remote_url})!"
+      end
     end
   end
 end
--- lib/multirepo/files/config-entry.rb
+++ lib/multirepo/files/config-entry.rb
@@ -4,6 +4,8 @@ require "multirepo/git/repo"
 module MultiRepo
   class ConfigEntry
     attr_accessor :folder_name
+    attr_accessor :remote_url
+    attr_accessor :branch_name
     attr_accessor :repo
     
     def to_s
@@ -21,27 +23,19 @@ module MultiRepo
     end
     
     def initialize_with_repo(repo)
-      @folder_name = repo.working_copy_basename
       @repo = repo
+      
+      @folder_name = repo.working_copy_basename
       @remote_url = repo.remote('origin').url
       @branch_name = repo.current_branch
     end
     
     def initialize_with_args(folder_name, remote_url, branch_name)
       @folder_name = folder_name
-      @repo = Repo.new("../#{folder_name}")
       @remote_url = remote_url
       @branch_name = branch_name
-    end
-    
-    def install
-      if @repo.exists?
-        check_repo_validity
-        fetch_repo
-      else
-        clone_repo
-      end
-      checkout_branch
+      
+      @repo = Repo.new("../#{folder_name}")
     end
     
     def exists?
@@ -49,46 +43,6 @@ module MultiRepo
         return true if line.start_with?(@folder_name)
       end
       false
-    end
-    
-    # Repo operations
-    
-    def fetch_repo
-      Console.log_substep("Working copy #{@repo.working_copy} already exists, fetching instead...")
-      if !@repo.fetch then raise "Could not fetch from remote #{@repo.remote('origin').url}"
-      end
-    end
-    
-    def clone_repo
-      Console.log_substep("Cloning #{@remote_url} to #{@repo.working_copy}")
-      if !@repo.clone(@remote_url) then raise "Could not clone remote #{@remote_url}" end
-    end
-    
-    def checkout_branch
-      branch = @repo.branch(@branch_name);
-      
-      unless branch.exists?
-        Console.log_warning("Branch #{@branch_name} doesn't exist in working copy #{@repo.working_copy}")
-        return
-      end
-      
-      if branch.checkout
-        Console.log_info("Checked out branch #{branch.name} -> origin/#{branch.name}")
-      else
-        raise "Could not setup branch #{branch.name}"
-      end
-    end
-    
-    # Validation
-    
-    def check_repo_validity
-      unless remote_matches_entry
-        raise "#{@folder_name} origin URL (#{@repo.remote('origin').url}) does not match entry (#{@remote_url})!"
-      end
-    end
-    
-    def remote_matches_entry
-      @repo.remote("origin").url == @remote_url
     end
   end
 end
