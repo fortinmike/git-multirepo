2610fc9ac88fe07a235e56f87460c8238e766d46
Michaël Fortin <fortinmike@irradiated.net>
2015-01-27 17:57:09 -0500

    Not checking for branch existence before checkout (the branch can be present only on origin, still needs to be checked out).

--- lib/multirepo/commands/install.rb
+++ lib/multirepo/commands/install.rb
@@ -49,15 +49,10 @@ module MultiRepo
     def checkout_branch(entry)
       branch = entry.repo.branch(entry.branch_name);
       
-      unless branch.exists?
-        Console.log_warning("Branch #{entry.branch_name} doesn't exist in working copy #{entry.repo.working_copy}")
-        return
-      end
-      
       if branch.checkout
         Console.log_substep("Checked out branch #{branch.name} -> origin/#{branch.name}")
       else
-        raise "Could not setup branch #{branch.name}"
+        raise "Could not checkout branch #{branch.name}"
       end
     end
     
