cbe970afac9c734b2b3be717e48a73f2d79a6b09
Michaël Fortin <fortinmike@irradiated.net>
2015-01-28 15:59:55 -0500

    Added multi remove.

---
+++ lib/multirepo/commands/remove.rb
@@ -0,0 +1,36 @@
+require "multirepo/utility/console"
+require "multirepo/files/config-file"
+
+module MultiRepo
+  class Remove < Command
+    self.command = "remove"
+    self.summary = "Removes the specified dependency from multirepo."
+    
+    def initialize(argv)
+      @path = argv.shift_argument
+      super
+    end
+    
+    def validate!
+      super
+      help! "You must provide a repo path to add as a dependency" unless @path
+    end
+    
+    def run
+      super
+      ensure_multirepo_initialized
+      
+      repo = Repo.new(@path)
+      entry = ConfigEntry.new(repo)
+      
+      if ConfigFile.entry_exists?(entry)
+        ConfigFile.remove_entry(entry)
+        Console.log_step("Removed #{@path} from the .multirepo file")
+      else
+        raise MultiRepoException, "#{@path} isn't tracked by multirepo"
+      end
+    rescue MultiRepoException => e
+      Console.log_error(e.message)
+    end
+  end
+end
