bb77547b1dfe4938185b87af4cbfe6b9c717ebc8
Michaël Fortin <fortinmike@irradiated.net>
2015-01-28 13:36:18 -0500

    Fixed add command raising if there was no path provided.

--- lib/multirepo/commands/add.rb
+++ lib/multirepo/commands/add.rb
@@ -7,8 +7,13 @@ module MultiRepo
     self.summary = "Add a dependency repository to the .multirepo file."
     
     def initialize(argv)
-      @repo = Repo.new(argv.shift_argument)
+      @path = argv.shift_argument
       super
+    end
+    
+    def validate!
+      super
+      help! "You must provide a repo path to add as a dependency" unless @path
     end
     
     def run
@@ -21,7 +26,8 @@ module MultiRepo
         Console.log_substep("Created missing .multirepo file")
       end
       
-      entry = ConfigEntry.new(@repo)
+      repo = Repo.new(@path)
+      entry = ConfigEntry.new(repo)
       if ConfigFile.entry_exists?(entry)
         Console.log_info("There is already an entry for #{entry.path} in the .multirepo file")
       else
@@ -34,8 +40,8 @@ module MultiRepo
     end
     
     def ensure_dependency_repo_exists
-      if !Dir.exists?(@repo.path) then raise MultiRepoException, "There is no folder at path #{@repo.path}" end
-      if !@repo.exists? then raise MultiRepoException, "#{@repo.path} is not a repository" end
+      if !Dir.exists?(@repo.path) then raise MultiRepoException, "There is no folder at path #{@path}" end
+      if !@repo.exists? then raise MultiRepoException, "#{@path} is not a repository" end
     end
   end
 end
