e4d3ffa4ada33d6d00c64e9aceb24ed616570889
Michaël Fortin <fortinmike@irradiated.net>
2015-02-01 20:41:36 -0500

    Using popen2e in Runner.

--- lib/multirepo/utility/runner.rb
+++ lib/multirepo/utility/runner.rb
@@ -8,28 +8,14 @@ module MultiRepo
     
     def self.run(cmd, show_output)
       output = []
-      Open3.popen3(cmd) do |stdin, stdout, stderr, thread|     
-        # STDOUT
-        Thread.new do
-          until (line = stdout.gets).nil? do
-            output << line
-            puts line if show_output
-          end
+      Open3.popen2e(cmd) do |stdin, stdout_and_stderr, thread|
+        stdout_and_stderr.each do |line|
+          puts line# if show_output
+          output << line
         end
-        
-        # STDERR
-        Thread.new do
-          until (line = stderr.gets).nil? do
-            output << line
-            puts line.red if show_output
-          end
-        end
-        
-        thread.join # don't exit until the external process is done
         @last_command_succeeded = thread.value.success?
       end
-      
-      return output.join("")
+      output.join("")
     end
   end
 end
