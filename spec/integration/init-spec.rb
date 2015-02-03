f434971cb4a14e4f16822f7663f7760b5e15ef9d
Michaël Fortin <fortinmike@irradiated.net>
2015-02-03 10:20:25 -0500

    Added a few pending tests in init-spec.

--- spec/integration/init-spec.rb
+++ spec/integration/init-spec.rb
@@ -1,7 +1,23 @@
 require "multirepo/commands/init"
 
 RSpec.describe("Init") do
-  it "runs a test" do
-    puts "test has run!"
+  it "creates the config file" do
+    pending
+  end
+  
+  it "adds only specified repos to the config file" do
+    pending
+  end
+  
+  it "stages the config file" do
+    pending
+  end
+  
+  it "installs the pre-commit hook" do
+    pending
+  end
+  
+  it "fails when there are uncommitted changes in dependencies" do
+    pending
   end
 end
