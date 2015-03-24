66e49dcdbc5fe316a05996728798827071be5cbf
Michaël Fortin <michael@simbioz.com>
2015-03-24 16:43:36 -0400

    Implemented directory creation and main project clone in CloneCommand.

--- lib/multirepo/commands/clone-command.rb
+++ lib/multirepo/commands/clone-command.rb
@@ -9,13 +9,13 @@ module MultiRepo
     
     def self.options
       [
-        ['[uri]', 'The repository to clone.']
-        ['[name]', 'The name of the containing folder that will be created. If unspecified, will be derived from the specified URL.']
+        ['[url]', 'The repository to clone.'],
+        ['[name]', 'The name of the containing folder that will be created.']
       ].concat(super)
     end
     
     def initialize(argv)
-      @uri = argv.shift_argument
+      @url = argv.shift_argument
       @name = argv.shift_argument
       super
     end
@@ -23,24 +23,26 @@ module MultiRepo
     def validate!
       super
       help! "You must specify a repository to clone" unless @url
+      help! "You must specify a containing folder name" unless @name
     end
 
     def run
-      super
       Console.log_step("Cloning...")
 
-      puts name_from_uri(@uri)
-      #Dir.mkdir(@name || name_from_uri(@uri))
-      
-      self.install_pre_commit_hook
-      
+      raise MultiRepoException, "A directory named #{@name} already exists" if Dir.exists?(@name)
+
+      main_repo_path = "#{@name}/#{@name}"
+
+      FileUtils.mkpath(main_repo_path)
+
+      main_repo = Repo.new(main_repo_path)
+      main_repo.clone(@url)
+
+      # TODO: perform multi install!
+            
       Console.log_step("Done!")
     rescue MultiRepoException => e
       Console.log_error(e.message)
-    end
-
-    def name_from_url(uri)
-      URI(uri).path.split('/').last
     end
   end
 end
