0e5334a69683c800a4eb259f2c6e302aa201f9c4
Michaël Fortin <fortinmike@irradiated.net>
2015-03-25 17:50:26 -0400

    91090676 : Fixed unwanted empty lines in Runner logging.

--- lib/multirepo/utility/runner.rb
+++ lib/multirepo/utility/runner.rb
@@ -17,7 +17,7 @@ module MultiRepo
       lines = []
       Open3.popen2e(cmd) do |stdin, stdout_and_stderr, thread|
         stdout_and_stderr.each do |line|
-          Console.log_info(line) if verbosity == Verbosity::ALWAYS_OUTPUT || Config.instance.verbose
+          Console.log_info(line.rstrip) if verbosity == Verbosity::ALWAYS_OUTPUT || Config.instance.verbose
           lines << line
         end
         @last_command_succeeded = thread.value.success?
