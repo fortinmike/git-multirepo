61e182e326ef38c3982fc4c09647e031be8c972e
Michaël Fortin <fortinmike@irradiated.net>
2015-04-02 09:50:29 -0400

    Implemented the prepare-commit-msg hook to perform appropriate checks and operations on pre-commit and pre-merge.

--- lib/multirepo/hooks/prepare-commit-msg-hook.rb
+++ lib/multirepo/hooks/prepare-commit-msg-hook.rb
@@ -8,14 +8,34 @@ module MultiRepo
     def self.run(argv)
       Config.instance.running_git_hook = true
       
-      dependencies_clean = Utils.ensure_dependencies_clean(ConfigFile.load)
+      case argv[1]
+      when "merge"; pre_merge
+      else; pre_commit
+      end
       
+      exit 0 # Success!
+    end
+    
+    def self.pre_commit
+      ensure_dependencies_clean
+      update_lock_file
+    end
+    
+    def self.pre_merge
+      ensure_dependencies_clean
+    end
+    
+    def self.ensure_dependencies_clean
+      dependencies_clean = Utils.ensure_dependencies_clean(ConfigFile.load)
       if !dependencies_clean
         Console.log_error("multirepo: You must commit changes to your dependencies before you can commit this repo")
         exit 1
       end
-      
-      exit 0 # Success!
+    end
+    
+    def self.update_lock_file
+      LockFile.update
+      Console.log_info("multirepo: Updated and staged lock file with current HEAD revisions for all dependencies")
     end
   end
 end
