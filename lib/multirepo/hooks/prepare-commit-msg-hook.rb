1baa87c58956ae61b0ca86cb0e7f8fb4199179f6
Michaël Fortin <fortinmike@irradiated.net>
2015-04-02 09:15:17 -0400

    There is no pre-merge hook! Working on moving the checks to prepare-commit-msg.

--- lib/multirepo/hooks/prepare-commit-msg-hook.rb
+++ lib/multirepo/hooks/prepare-commit-msg-hook.rb
@@ -4,14 +4,14 @@ require "multirepo/utility/utils"
 require "multirepo/utility/console"
 
 module MultiRepo
-  class PreMergeHook
-    def self.run
+  class PrepareCommitMsgHook
+    def self.run(argv)
       Config.instance.running_git_hook = true
       
       dependencies_clean = Utils.ensure_dependencies_clean(ConfigFile.load)
       
       if !dependencies_clean
-        Console.log_error("multirepo: You must commit changes to your dependencies before you can merge this repo")
+        Console.log_error("multirepo: You must commit changes to your dependencies before you can commit this repo")
         exit 1
       end
       
