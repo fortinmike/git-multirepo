ec7e11fbb97f7a249e7a9d09764bb1c362e40ceb
Michaël Fortin <fortinmike@irradiated.net>
2015-05-01 22:12:17 -0400

    Fixed exception in TrackingFile.update_internal when the updated file didn't exist already.

--- lib/multirepo/files/tracking-file.rb
+++ lib/multirepo/files/tracking-file.rb
@@ -1,7 +1,7 @@
 module MultiRepo
   class TrackingFile
     def self.update_internal(file, new_content)
-      old_content = File.read(file)
+      old_content = File.exists?(file) ? File.read(file) : nil
       File.write(file, new_content)
       return new_content != old_content
     end
