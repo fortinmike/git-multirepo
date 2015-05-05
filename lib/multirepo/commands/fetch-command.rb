4dfdd3eea28ce9168adfce56ec1ad704cf3576c5
Michaël Fortin <fortinmike@irradiated.net>
2015-05-04 20:50:57 -0400

    93745800 : FetchCommand also fetches the main repo.

--- lib/multirepo/commands/fetch-command.rb
+++ lib/multirepo/commands/fetch-command.rb
@@ -10,6 +10,12 @@ module MultiRepo
       ensure_in_work_tree
       ensure_multirepo_enabled
       
+      Console.log_step("Fetching main repo...")
+      
+      main_repo = Repo.new(".")
+      Console.log_substep("Fetching from #{main_repo.remote('origin').url}...")
+      main_repo.fetch
+      
       Console.log_step("Fetching dependencies...")
       
       ConfigFile.load_entries.each do |entry|
