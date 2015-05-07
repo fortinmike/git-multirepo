4e921a616f7266733db6c1680fa651ce38358cb6
Michaël Fortin <fortinmike@irradiated.net>
2015-05-07 08:31:23 -0400

    Added revision selection options to MergeCommand.

--- lib/multirepo/commands/merge-command.rb
+++ lib/multirepo/commands/merge-command.rb
@@ -1,5 +1,7 @@
 require "multirepo/utility/console"
 require "multirepo/logic/node"
+require "multirepo/logic/revision-selector"
+require "multirepo/logic/performer"
 
 module MultiRepo
   class MergeCommand < Command
@@ -7,17 +9,24 @@ module MultiRepo
     self.summary = "Performs a git merge on all dependencies, in the proper order."
     
     def self.options
-      [['<ref>', 'The main repo tag, branch or commit id to merge.']].concat(super)
+      [
+        ['<ref>', 'The main repo tag, branch or commit id to merge.'],
+        ['[--latest]', 'Merge the HEAD of each stored dependency branch instead of the commits recorded in the lock file.'],
+        ['[--exact]', 'Merge the exact specified ref for each repo, regardless of what\'s stored in the lock file.']
+      ].concat(super)
     end
     
     def initialize(argv)
       @ref = argv.shift_argument
+      @checkout_latest = argv.flag?("latest")
+      @checkout_exact = argv.flag?("exact")
       super
     end
     
     def validate!
       super
       help! "You must specify a ref to merge" unless @ref
+      help! "You can't provide more than one operation modifier (--latest, --exact, etc.)" if @checkout_latest && @checkout_exact
     end
     
     def run
