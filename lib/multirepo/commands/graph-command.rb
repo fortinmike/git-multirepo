be899dfa31168a3df64da0e4a7a139e4e14b563f
Michaël Fortin <fortinmike@irradiated.net>
2015-05-15 00:22:59 -0400

    Initial version of multi graph command.

---
+++ lib/multirepo/commands/graph-command.rb
@@ -0,0 +1,43 @@
+require "os"
+require "graphviz"
+
+require "multirepo/utility/console"
+require "multirepo/logic/node"
+
+module MultiRepo
+  class GraphCommand < Command
+    self.command = "graph"
+    self.summary = "Graphs the dependency tree from the current multirepo-enabled repo."
+    
+    def run
+      super
+      ensure_in_work_tree
+      ensure_multirepo_enabled
+      
+      root = Node.new(".")
+      graph = GraphViz.new(:G, :type => :digraph)
+      build_graph_recursive(graph, root)
+      
+      path = File.expand_path("~/Desktop/graph.png")
+      
+      begin
+        graph.output(:png => path)
+      rescue StandardError => e
+        Console.log_error(e.message)
+      end
+      
+      Console.log_step("Generated graph image #{path}")
+    rescue MultiRepoException => e
+      Console.log_error(e.message)
+    end
+    
+    def build_graph_recursive(graph, node)
+      parent_graph_node = graph.add_nodes(node.name)
+      node.children.each do |child_node|
+        child_graph_node = graph.add_nodes(child_node.name)
+        graph.add_edges(parent_graph_node, child_graph_node)
+        build_graph_recursive(graph, child_node)
+      end
+    end
+  end
+end
