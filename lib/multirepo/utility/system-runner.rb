5d7a18c80e2ab665d69504554c291bdd7092196d
Michaël Fortin <fortinmike@irradiated.net>
2015-06-03 23:23:38 -0400

    Fixed Style/SpecialGlobalVars offenses.

--- lib/multirepo/utility/system-runner.rb
+++ lib/multirepo/utility/system-runner.rb
@@ -6,7 +6,7 @@ module MultiRepo
       Console.log_info("Command: #{cmd}") if Config.instance.verbose
       
       output = system(cmd)
-      last_command_succeeded = ($?.exitstatus == 0)
+      last_command_succeeded = ($CHILD_STATUS.exitstatus == 0)
       
       return output, last_command_succeeded
     end
