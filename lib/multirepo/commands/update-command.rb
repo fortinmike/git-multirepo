e6fc0b2ccc59d63e43e297ba40134efd1a164ef6
Michaël Fortin <michael@simbioz.com>
2015-06-05 15:41:13 -0400

    96311404 : Added --deps, --main and --all to UpdateCommand, update all by default.

--- lib/multirepo/commands/update-command.rb
+++ lib/multirepo/commands/update-command.rb
@@ -9,22 +9,33 @@ module MultiRepo
     
     def self.options
       [
+        ['[--all]', 'Update the main repository and all dependencies.'],
+        ['[--main]', 'Update the main repository.'],
+        ['[--deps]', 'Update dependencies.'],
         ['[--force]', 'Update the tracking files even if dependencies contain uncommitted changes.'],
         ['[--commit]', 'Commit the tracking files after updating them.']
       ].concat(super)
     end
     
     def initialize(argv)
+      @all = argv.flag?("all")
+      @main_only = argv.flag?("main")
+      @deps_only = argv.flag?("deps")
       @commit = argv.flag?("commit")
       @force = argv.flag?("force")
       super
     end
 
+    def validate!
+      super
+      unless validate_only_one_flag(@all, @main_only, @deps_only)
+        help! "You can't provide more than one operation modifier (--deps, --main, etc.)"
+      end
+    end
+
     def run
       ensure_in_work_tree
       ensure_multirepo_enabled
-      
-      Console.log_step("Updating...")
       
       dependencies_clean = Utils.dependencies_clean?(ConfigFile.new(".").load_entries)
       if dependencies_clean || @force
@@ -37,11 +48,28 @@ module MultiRepo
     end
     
     def update_tracking_files_step
+      if @main_only
+        Console.log_step("Updating main repo...")
+        update_main
+      elsif @deps_only
+        Console.log_step("Updating dependencies...")
+        update_dependencies
+      else
+        Console.log_step("Updating main repo and dependencies...")
+        update_dependencies
+        update_main
+      end
+    end
+
+    def update_dependencies
       Performer.dependencies.each do |dependency|
         path = dependency.config_entry.path
         name = dependency.config_entry.name
         update_tracking_files(path, name) if Utils.multirepo_enabled?(path)
       end
+    end
+
+    def update_main
       update_tracking_files(".", "main repo")
     end
     
